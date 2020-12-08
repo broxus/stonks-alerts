f464104818a22633f96939af038f5ece48531e3e
Ivan Kalinin <i.kalinin@dexpa.io>
2020-12-08 16:01:37 +0300

    Improve error messages

--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,62 @@ mod listener;
 mod settings;
 mod state;
 
+use std::str::FromStr;
 use std::sync::Arc;
 
 use settings::Settings;
 use teloxide::prelude::*;
 use teloxide::types::ParseMode;
-use teloxide::utils::command::BotCommand;
+use teloxide::utils::command::{BotCommand, ParseError};
 
 use crate::listener::*;
 use crate::state::*;
+
+struct Subscription {
+    direction: Direction,
+    address: String,
+}
+
+fn parse_subscription(input: String) -> Result<(Subscription,), ParseError> {
+    let mut parts = input.trim().split(' ').filter(|s| !s.is_empty());
+
+    let description = "Usage:\n    `/subscribe DIRECTION ADDRESS`\n\nWhere:\n    `DIRECTION` \\- `all`, `incoming`, `outgoing`\n    `ADDRESS` \\- raw or packed address";
+
+    match parts.next() {
+        Some(first) => match parts.next() {
+            Some(second) => match Direction::from_str(first) {
+                Ok(direction) => Ok((Subscription {
+                    direction,
+                    address: second.to_string(),
+                },)),
+                Err(e) => Err(ParseError::IncorrectFormat(
+                    format!("{}\n\n{}", e.to_string(), description).into(),
+                )),
+            },
+            None => Ok((Subscription {
+                direction: Direction::All,
+                address: first.to_owned(),
+            },)),
+        },
+        None => Err(ParseError::Custom(description.to_owned().into())),
+    }
+}
 
 #[derive(BotCommand)]
 #[command(rename = "lowercase", description = "These commands are supported:")]
 enum Command {
     #[command(description = "display this text")]
     Start,
-    #[command(description = "subscribe to account", parse_with = "split")]
-    Subscribe {
-        direction: Direction,
-        address: String,
-    },
-    #[command(description = "unsubscribe from account", parse_with = "split")]
-    Unsubscribe {
-        direction: Direction,
-        address: String,
-    },
+    #[command(
+        description = "subscribe to account",
+        parse_with = "parse_subscription"
+    )]
+    Subscribe(Subscription),
+    #[command(
+        description = "unsubscribe from account",
+        parse_with = "parse_subscription"
+    )]
+    Unsubscribe(Subscription),
     #[command(description = "List subscriptions")]
     List,
 }
@@ -74,16 +105,14 @@ async fn run() -> anyhow::Result<()> {
                 None => return Ok(()),
             };
 
-            log::info!("text: {}", text);
-
             match Command::parse(text, &bot_name) {
                 Ok(Command::Start) => {
                     cx.answer(Command::descriptions()).send().await?;
                 }
-                Ok(Command::Subscribe { direction, address }) => {
-                    match state.insert(&address, direction, chat_id) {
+                Ok(Command::Subscribe(subscription)) => {
+                    match state.insert(&subscription.address, subscription.direction, chat_id) {
                         Ok(_) => {
-                            cx.reply_to(format!("Subscribed to:\n`{}`", address))
+                            cx.reply_to(format!("Subscribed to:\n`{}`", subscription.address))
                                 .parse_mode(ParseMode::MarkdownV2)
                                 .send()
                                 .await?;
@@ -96,10 +125,10 @@ async fn run() -> anyhow::Result<()> {
                         }
                     }
                 }
-                Ok(Command::Unsubscribe { direction, address }) => {
-                    match state.remove(&address, direction, chat_id) {
+                Ok(Command::Unsubscribe(subscription)) => {
+                    match state.remove(&subscription.address, subscription.direction, chat_id) {
                         Ok(_) => {
-                            cx.reply_to(format!("Unsubscribed from:\n`{}`", address))
+                            cx.reply_to(format!("Unsubscribed from:\n`{}`", subscription.address))
                                 .parse_mode(ParseMode::MarkdownV2)
                                 .send()
                                 .await?;
@@ -125,8 +154,7 @@ async fn run() -> anyhow::Result<()> {
                         .await?;
                 }
                 Err(e) => {
-                    let mut response = e.to_string();
-                    cx.reply_to(response)
+                    cx.reply_to(e.to_string())
                         .parse_mode(ParseMode::MarkdownV2)
                         .send()
                         .await?;
